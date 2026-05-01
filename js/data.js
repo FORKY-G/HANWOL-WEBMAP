9e7ca560b13aeee69cc98bab21505af7faffacae
FORKY-G <forky.yun@gmail.com>
2026-05-01 18:01:58 +0900

    Update data.js

--- js/data.js
+++ js/data.js
@@ -3,18 +3,18 @@ const spawnData = { name: "스폰 지점", mcX: -969, mcZ: -965 };
 
 //2. 십이지신
 const animals = [
-    { order: 1, name: "쥐", mcX: -2900, mcY: 127, mcZ: -4600 },
-    { order: 2, name: "소", mcX: -616, mcY: 116, mcZ: 3889 },
-    { order: 3, name: "호랑이", mcX: -5500, mcY: 152, mcZ: -1600 },
-    { order: 4, name: "토끼", mcX: 4590, mcY: 122, mcZ: -3397 },
-    { order: 5, name: "용", mcX: -891, mcY: 106, mcZ: -536 },
-    { order: 6, name: "뱀", mcX: 2685, mcY: 145, mcZ: -4051 },
-    { order: 7, name: "말", mcX: -2365, mcY: 282, mcZ: 3968 },
-    { order: 8, name: "양", mcX: 5093, mcY: 135, mcZ: 2092 },
-    { order: 9, name: "원숭이", mcX: 3280, mcY: 162, mcZ: 2952 },
-    { order: 10, name: "닭", mcX: 5075, mcY: 106, mcZ: -344 },
-    { order: 11, name: "개", mcX: -4455, mcY: 171, mcZ: -2376 },
-    { order: 12, name: "돼지", mcX: -3657, mcY: 212, mcZ: 1112 }
+    { order: 1, name: "쥐", mcX: -2900, mcY: 127, mcZ: -4600, file: "zodiac1.png" },
+    { order: 2, name: "소", mcX: -616, mcY: 116, mcZ: 3889, file: "zodiac2.png" },
+    { order: 3, name: "호랑이", mcX: -5500, mcY: 152, mcZ: -1600, file: "zodiac3.png" },
+    { order: 4, name: "토끼", mcX: 4590, mcY: 122, mcZ: -3397, file: "zodiac4.png" },
+    { order: 5, name: "용", mcX: -891, mcY: 106, mcZ: -536, file: "zodiac5.png" },
+    { order: 6, name: "뱀", mcX: 2685, mcY: 145, mcZ: -4051, file: "zodiac6.png" },
+    { order: 7, name: "말", mcX: -2365, mcY: 282, mcZ: 3968, file: "zodiac7.png" },
+    { order: 8, name: "양", mcX: 5093, mcY: 135, mcZ: 2092, file: "zodiac8.png" },
+    { order: 9, name: "원숭이", mcX: 3280, mcY: 162, mcZ: 2952, file: "zodiac9.png" },
+    { order: 10, name: "닭", mcX: 5075, mcY: 106, mcZ: -344, file: "zodiac10.png" },
+    { order: 11, name: "개", mcX: -4455, mcY: 171, mcZ: -2376, file: "zodiac11.png" },
+    { order: 12, name: "돼지", mcX: -3657, mcY: 212, mcZ: 1112, file: "zodiac12.png" }
 ];
 
 //3. 광산
